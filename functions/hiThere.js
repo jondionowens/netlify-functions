1c066fef5f16f5d3ea81bfae32d93b46ce7ea6d4
Jonathan Owens <jonathano@metia.com>
2019-07-23 15:46:20 -0700

    Seeing if I can just get data back

--- functions/hiThere.js
+++ functions/hiThere.js
@@ -1,6 +1,8 @@
 exports.handler = (event, content, callback) => {
   callback(null, {
     statusCode: 200,
-    body: "<html><body>Hello, World</body></html>"
-    });
+    body: {
+      'hello': 'there'
+    }
+  });
 }
